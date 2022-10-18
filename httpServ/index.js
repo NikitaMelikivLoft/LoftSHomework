9369b8d734fbefc6ee7886aaa7a3ac911bd7884e
Nikita <domiks2228@gmail.com.com>
2022-10-18 22:54:06 +0300

    http

--- httpServ/index.js
+++ httpServ/index.js
@@ -2,40 +2,49 @@ const express = require("express")
 
 const app=express()
 
-const limitTime=+process.env.EXT || 20;
-const Delay= +process.env.INT || 1000;
+const limitTime=process.env.EXT || 20;
+const Delay= process.env.INT || 1000;
 const PORT = process.env.PORT || 3000;
 
 let connect=[];
 
 app.get("/date",(req,res,next)=>{
-    res.setHeader("Content-Type","text/html; charset=utf-8");
-    res.setHeader("Transfer-Encoding","chunked")
-    connect.push(res)
+  res.setHeader("Content-Type","text/html; charset=utf-8");
+  res.setHeader("Transfer-Encoding","chunked")
+  connect.push(res)
+  setTimeout(run,Delay);
+
 });
 
-let tick=0;
-setTimeout(function run(){
-  const date = new Date().toUTCString();
-  console.log(date);
-  if(++tick>limitTime){
-        connect.map(res=>{
-        res.write("END\n");
-        res.end();
-    })
-    connect=[];
-    tick=0;
-    const date = new Date().toUTCString();
-    console.log(`Время отключения сервера ${date} `);
-  }
 
-  connect.map((res,i)=>{
-    res.write(`Hello ${i} person! Date: ${date}.\n`);
-  })
-  setTimeout(run, Delay);
-},Delay);
+let tick=0;
+function run(){
+    const date = new Date().toUTCString();
+    console.log(date);
+   
+    if(++tick>limitTime){
+          connect.map(res=>{
+          res.write("END\n");
+          res.end();
+      })
+      connect=[];
+      tick=0;
+      const date = new Date().toUTCString();
+     
+    }
+  
+    connect.map((res,i)=>{
+      res.write(`Hello ${i} person! Date: ${date}.\n`);
+    })
+    setTimeout(run,Delay);
+  
+
+  } 
+  
+ 
+
 app.listen(PORT, ()=>{
     console.log(`Server is runnung on port ${PORT}`);
     console.log(`Server delay ${Delay}`);
     console.log(`Время работы сервера ${limitTime} секунд`);
-})
+});
